451813b3f5e2c539fd233db2eab940d117adb082
Loki <bounthienglocky@gmail.com>
2019-09-25 10:05:29 +0200

    essais material et non material

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,39 @@
 import 'package:flutter/material.dart';
 
+void main() => runApp(MyApp());
 
+class MyApp extends StatelessWidget {
+  @override
+  
+  // Widget build(BuildContext context) {
+  //   return MaterialApp(
+  //     title: 'Layout demo',
+  //     home: Scaffold(
+  //       appBar: AppBar(
+  //         title: Text('Flutter layout'),
+  //       ),
+  //       body: Center(
+  //         child: Text(
+  //         'Hello ass!'
+  //       ),
+  //       )
+  //     )
 
+  //   );
+  // }
+ Widget build(BuildContext context) {
+    
+    return Container(
+      decoration: BoxDecoration(color: Colors.green),
+      child: Center(
+        child: Text('Halo',
+        textDirection: TextDirection.ltr,
+        style: TextStyle(
+          
+          fontSize: 32,
+          color: Colors.white,
+        )),
+        )
+     ,);
+  }
+}
