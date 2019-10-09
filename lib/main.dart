40609dc581fa68984c20bdbf5c8ddc326834b9dd
Loki <bounthienglocky@gmail.com>
2019-10-09 03:40:12 +0200

    added animation logo

--- lib/main.dart
+++ lib/main.dart
@@ -4,6 +4,25 @@ import 'package:flutter/rendering.dart';
 
 void main() => runApp(MonApp());
 
+class AnimatedLogo extends AnimatedWidget {
+  AnimatedLogo({Key key, Animation<double> animation}):
+   super(key: key, listenable: animation);
+
+  
+  Widget build(BuildContext context) {
+    final Animation<double> animation = listenable as Animation<double>;
+    return Center(child: Container(child: 
+    FlutterLogo(),
+    margin: EdgeInsets.symmetric(vertical: 10),
+    width: animation.value,
+    height: animation.value
+    ,),);
+  }
+}
+
+class LogoWidget extends StatelessWidget{
+  Widget build(BuildContext context)=> Container(margin: EdgeInsets.symmetric(vertical: 10),child:FlutterLogo());
+}
 class MonApp extends StatefulWidget{
   _MonAppState createState()=> _MonAppState();
 }
@@ -16,34 +35,50 @@ class _MonAppState extends State<MonApp> with SingleTickerProviderStateMixin{
   void initState() {
     super.initState();
     controller = AnimationController(
-      duration: Duration(seconds: 2),vsync: this);
-    animation= Tween<double>(begin: 0,end:300).animate(controller)
-      ..addListener((){
-        setState(() {
-          
-        });
-      }); 
+      duration: Duration(seconds: 1),vsync: this);
+
+    animation= Tween<double>(begin: 80,end:100).animate(controller);
+    //pour boucler l'animation
+    // ..addStatusListener((status){
+    //   if (status == AnimationStatus.completed){
+    //     controller.reverse();
+    //   }else if (status == AnimationStatus.dismissed){
+    //     controller.forward();
+    //   }
+    // })
+    // ..addStatusListener((state)=>print('$state'));
     controller.forward();
   }
 
 
   @override
-  Widget build(BuildContext context) {
-    
-    return Center(
-      child: Container(
-        color: Colors.black,
-        width: animation.value,
-        height: animation.value,
-        child:FlutterLogo()   
-    ,)
-    ,);
-  }
+  //Widget build(BuildContext context) => AnimatedLogo(animation:animation);
+  Widget build(BuildContext context) => GrowTransition(
+    child:LogoWidget(),
+    animation: animation,
+  );
+
   @override
   void dispose() {
-    controller.dispose();
     super.dispose();
+    controller.dispose();
+    
   }
 
 }
 
+class GrowTransition extends StatelessWidget {
+  GrowTransition({this.child, this.animation});
+
+  final Widget child;
+  final Animation<double> animation;
+
+  Widget build(BuildContext context) => Center(child: AnimatedBuilder(
+    animation: animation,
+    builder: (context,child)=>Container(height: animation.value,
+    width: animation.value,
+    child:child,),
+    child:child,
+  )
+  );
+}
